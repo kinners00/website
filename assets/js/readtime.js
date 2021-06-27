5e8f4d48bcb2ef7ecf71f6359a7f836cfdeaea89
Marc McKinley <marcmckinley@me.com>
2021-06-27 22:52:56 +0100

    push

--- assets/js/readtime.js
+++ assets/js/readtime.js
@@ -1,13 +1,15 @@
-let test = jQuery.get('https://raw.githubusercontent.com/kinners00/website/development/test.md', function(data) {
+$.get("test.md", function(response) {
+  var test = response;
+
+  const wordsPerMinute = 200; // Average case.
+  let result;
+
+  let textLength = test.value.split(" ").length; // Split by words
+  if(textLength > 0){
+    let value = Math.ceil(textLength / wordsPerMinute);
+    result = `~${value} min read`;
+  }
+  document.getElementById("readingTime").innerText = result;
 });
 
-      const wordsPerMinute = 200; // Average case.
-      let result;
-  
-      let textLength = test.value.split(" ").length; // Split by words
-      if(textLength > 0){
-        let value = Math.ceil(textLength / wordsPerMinute);
-        result = `~${value} min read`;
-      }
-      document.getElementById("readingTime").innerText = result;
 
