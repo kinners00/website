c636e597a415249a662c7070fc05f68bbd972fe6
Marc McKinley <marcmckinley@me.com>
2021-06-27 22:37:30 +0100

    push

--- assets/js/readtime.js
+++ assets/js/readtime.js
@@ -1,7 +1,7 @@
- result = document.getElementsByClassName("markdown-body")
+
       const wordsPerMinute = 200; // Average case.
       let result;
-      
+      result = document.getElementsByClassName("markdown-body")
       let textLength = ev.value.split(" ").length; // Split by words
       if(textLength > 0){
         let value = Math.ceil(textLength / wordsPerMinute);
