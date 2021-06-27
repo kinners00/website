9e0409cb26590744bbf4ba882034098b9f3502ff
Marc McKinley <marcmckinley@me.com>
2021-06-27 21:05:39 +0100

    push

--- assets/js/readtime.js
+++ assets/js/readtime.js
@@ -1,10 +1,9 @@
 function readingTime() {
-    const text = this.shadowRoot.getElementByClass('markdown-body').innerText;
-    document.getElementById("time").innerText = time;
+    const text = this.shadowRoot.getElementByClass("markdown-body").innerText;
     const wpm = 225;
     const words = text.trim().split(/\s+/).length;
     const time = Math.ceil(words / wpm);
-
+    document.getElementById("time").innerText = time;
   // document.write(text).innerText = time;
   }
   readingTime();
